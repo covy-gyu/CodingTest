7322e096a00a6a706866eacac79ab9b504c0ce22
Gyucheol <74587816+covy-gyu@users.noreply.github.com>
2024-11-27 17:51:37 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.04 ms, Memory: 4.04 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.cpp
@@ -5,29 +5,28 @@
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<int> moves) {
-    size_t row = board.size();
-    size_t moves_sz = moves.size();
-    int answer = 0;
-    stack<int> st;
-    
-    for(int i =0; i<moves_sz; i++){
-        int col = moves[i]-1;
-        for(int j=0; j<row; j++){
-            int target = board[j][col];
-            if(target){
-                if(!st.empty()){
-                    if(st.top() == target){
-                        st.pop();        
-                        answer+=2;
-                    }
-                    else st.push(target);
+    size_t row = board.size();        // 보드의 행 개수
+    int answer = 0;                   // 터뜨린 인형의 개수
+    stack<int> st;                    // 바구니 역할 스택
+
+    for (size_t i = 0; i < moves.size(); i++) {
+        int col = moves[i] - 1;       // 1-based index를 0-based로 변환
+
+        for (size_t j = 0; j < row; j++) {
+            if (board[j][col] != 0) {  // 해당 칸에 인형이 있는 경우
+                int target = board[j][col]; // 인형 값
+                board[j][col] = 0;          // 인형 제거
+                
+                if (!st.empty() && st.top() == target) {
+                    st.pop();               // 동일한 인형 제거
+                    answer += 2;            // 제거한 인형은 2개
+                } else {
+                    st.push(target);        // 바구니에 인형 추가
                 }
-                else st.push(target);
-                board[j][col] = 0;
-                break;
+                break;                      // 해당 열 탐색 종료
             }
         }
     }
-
-    return answer;
+    
+    return answer; // 터뜨린 인형 개수 반환
 }
