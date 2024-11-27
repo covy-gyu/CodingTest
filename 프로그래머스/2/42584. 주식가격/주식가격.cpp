622537a3de83eee4537329e45d87c90323e76e0a
Gyucheol <74587816+covy-gyu@users.noreply.github.com>
2024-11-27 16:01:42 +0900

    [level 2] Title: 주식가격, Time: 28.55 ms, Memory: 26.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -1,22 +1,15 @@
-#include <string>
 #include <vector>
-
 using namespace std;
-
 vector<int> solution(vector<int> prices) {
-    size_t prices_sz = prices.size();
-    
-    vector<int> answer(prices_sz,0);
-    
-    for(int i =0; i< prices_sz - 1; i++){
+    vector<int> answer(prices.size(),0);
+    for(int i =0; i< prices.size() - 1; i++){
         int time = 0;
-        for(int j = i + 1; j < prices_sz; j++){
+        for(int j = i + 1; j < prices.size(); j++){
             answer[i] = j-i;
             if(prices[i] > prices[j]){
                 break;
             }
         }
     }
-    
     return answer;
 }
