359162ffee56db256afa691def2d308e826aa011
Gyu Cheol, KIM <74587816+covy-gyu@users.noreply.github.com>
2025-09-14 23:28:52 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N{0};
+    cin >> N;
+    for(int i = 1; i <= N ; i++){
+        for(int j = i; j <= N-1; j++){
+            cout << " ";
+        }
+        for(int k = 1; k <= i; k++){
+            cout << "*";
+        }
+       cout << endl;
+    }
+}
